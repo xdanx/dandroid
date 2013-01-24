e5eab27ca5fc1c5bc0cce44c2032eaf73a8f406a
Dan Octavian <dan@dan_acer.(none)>
2013-01-24 22:24:28 +0000

    added measured values of final robot position

--- practical2/calc_covariance.cpp
+++ practical2/calc_covariance.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    int n = 2;
+    int n = 10;
 
     /*int x[n], y[n];
     for(int i = 0; i < n; i++)
@@ -12,12 +12,23 @@ int main(int argc, char *argv[])
          x[i] = atoi(argv[i+1].c_str());
          y[i] = atoi(argv[i+1+n].c_str());
     }*/
+/*
+Pos = [(3.1, 0.4),
+              (2.8, -0.7),
+             (4.1, 0),
+             (2, 1.4),
+             (3.4, 1.2),
+             (4.1, 0),
+             (2.4, 0.6),
+             (4.5, 1.2),
+             (2.6, 0.5),
+             (1.8, 0.6)
+*/
+    float x[] = {3.1, 2.8, 4.1, 2, 3.4, 4.1, 2.4, 4.5, 2.6, 1.8};
+    float y[] = {0.4, -0.7, 0, 1.4, 1.2, 0, 0.6, 1.2, 0.5, 0.6};
 
-    int x[] = {1, 1};
-    int y[] = {1, 1};
-
-    int sx = 0;
-    int sy = 0;
+    float sx = 0;
+    float sy = 0;
 
     for(int i = 0; i < n; i++)
     {
@@ -29,7 +40,7 @@ int main(int argc, char *argv[])
 
     float p[4];
 
-    int s1 = 0, s2 = 0, s3 = 0;
+    float s1 = 0, s2 = 0, s3 = 0;
     for(int i = 0; i < n; i++)
     {
     	s1 += (x[i] - xm) * (x[i] - xm);
