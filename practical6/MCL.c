4682ad61b4dc24bd21649a8069f84dfbc45baffa
DaN <dan@apexis.ro>
2013-02-25 02:43:33 +0000

    Found the bug! It is pointed by BUG HERE!!
    Dan

--- practical6/MCL.c
+++ practical6/MCL.c
@@ -96,7 +96,9 @@ void position_add_angle(Position* p, float deltaAngle) {
 }
 
 void rotate(float degs) {
+	stop();
 	robotState = ROTATE_STATE;
+	PlaySound(soundDownwardTones);
 
 	/* Might go wrong, opposite to what we have in our debug msg */
 	int dir = (degs >= 0) - (degs < 0);
@@ -131,7 +133,7 @@ float calculate_likelihood(float x, float y, float theta, float z)
 {
 
 	int closestWallIndex = -1;
-	float shortestDistance = 0.0;
+	float shortestDistance = 20000.0;
 	float m;
 	int i = 0;
 
@@ -144,22 +146,19 @@ float calculate_likelihood(float x, float y, float theta, float z)
 		float px = x + m*cosDegrees(theta);
 		float py = y + m*sinDegrees(theta);
 
-		if((px >= min(a_x,b_x) && px <= max(a_x,b_x) && py >= min(a_y,b_y) && py <= max(a_y,b_y))) {
-			if( closestWallIndex == -1)
+		if(px >= min(a_x,b_x) && px <= max(a_x,b_x) && py >= min(a_y,b_y) && py <= max(a_y,b_y) && m >= 0)
+		{
+			if ( m <= shortestDistance )
 			{
-				closestWallIndex = i;
 				shortestDistance = m;
-		  } else
-		  {
-		  	if(m < shortestDistance && m >= 0) {
-		  		closestWallIndex = i;
-		  		shortestDistance = m;
-		  	}
-		  }
+				closestWallIndex = i;
+			}
 		}
 	}
 
-	float likelihood = exp(-pow(z-shortestDistance, 2) / (2 * pow(0.44, 2)));
+	//writeDebugStream("[calculate_likelihood]: shortestDistance: %f \n", shortestDistance);
+	//wait1Msec(10);
+	float likelihood = exp(-pow(z - shortestDistance, 2) * 1.0 / (2 * pow(0.44, 2))) + 0.1;
 	return likelihood;
 
 }
@@ -310,7 +309,9 @@ void resample()
 
 		// Search value of random1 in cumulativeWeightArray
 		int index = binary_search (random1);
-		writeDebugStream("[Resample] random_nr: %d, random1: %f, BS: %d\n",random_nr, random1 ,index);
+		if (index == 100 )
+			index = 99;
+		//writeDebugStream("[Resample] random_nr: %d, random1: %f, BS: %d\n",random_nr, random1 ,index);
 		// Copy new particle
 		xArrayCopy[i] = xArray[index];
 		yArrayCopy[i] = yArray[index];
@@ -332,22 +333,24 @@ void navigate_to_waypoint(float x, float y)
 	float med_x = 0, med_y = 0, med_theta = 0;
 	float i, z;
 
+	writeDebugStream("Going towards point %f, %f\n",x,y);
+
 	if (DEBUG)
 	{
 		print_10_points();
 		print_10_cwa();
 	}
+
+    /* !!!!!!!!!!!!!!!!!! BUG HERE !!!!!!!!!!!!!!!!!!!!! */
 	// estimate current posistion
 	for (i=0; i < NUMBER_OF_PARTICLES; ++i)
 	{
-		// Next time we will use , now it is just hard-coded.
-		// med_x += weightArray[i] * xArray[i];
 		med_x += xArray[i]*weightArray[i];
 		med_y += yArray[i]*weightArray[i];
 		med_theta += thetaArray[i]*weightArray[i];
 	}
-
 	writeDebugStream("Averages: x: %f y:%f theta:%f\n", med_x, med_y, med_theta);
+    /* !!!!!!!!!!!!!!!!!! END BUG HERE !!!!!!!!!!!!!!!!!!!!! */
 
 	// calculate difference
 	float dif_x = x - med_x; // dest - curr_pos
@@ -380,9 +383,11 @@ void navigate_to_waypoint(float x, float y)
 	rotate_degs = normalize_angle_value(rotate_degs - med_theta);
 	writeDebugStream("Rotate angle: %f\n",rotate_degs);
 
+	print_10_points();
 	// Rotate towards the correct position
 	rotate(rotate_degs);
 	points_update(rotate_degs, ROTATE_STATE);
+	print_10_points();
 
 	// move forward
 	float move_distance = sqrt(dif_x * dif_x + dif_y * dif_y);
@@ -397,13 +402,21 @@ void navigate_to_waypoint(float x, float y)
 
 	// Measure with sonar
 	z = SensorValue[sonar];
+	writeDebugStream("[Move_to_waypoint] %f\n",z);
+	writeDebugStream("Before Calculate likelihood\n");
+	print_10_cwa();
 	for ( i = 0; i < NUMBER_OF_PARTICLES; i++ )
 	{
 		weightArray[i] = calculate_likelihood(xArray[i], yArray[i], thetaArray[i], z);
 	}
 
+	writeDebugStream("Before Resampling\n");
+	print_10_cwa();
 	// Resampling
 	resample();
+	writeDebugStream("After Resampling\n");
+	print_10_cwa();
+
 }
 
 void set_starting_position(float x, float y, float theta)
@@ -468,16 +481,50 @@ task main() {
 	bPlaySounds = true;
 	set_starting_position(84.0, 30.0, 0.0);
 
-	int noWaypoints = 11;
-	int waypointXarray[11] = {104, 124, 144 ,180, 180, 126, 126, 126, 30, 84, 84};
-	int waypointYarray[11] = { 30,  30,  30,  30,  54,  54, 168, 126, 54, 54, 30};
+	drawMap();
 
 	StartTask(vehicle_compute_position);
 	StartTask(vehicle_draw_position);
 
-	for(int i = 0; i < noWaypoints; i++) {
-		navigate_to_waypoint(waypointXarray[i], waypointYarray[i]);
-	}
+	navigate_to_waypoint(104, 30);
+	drawParticles();
+	navigate_to_waypoint(124, 30);
+	drawParticles();
+	navigate_to_waypoint(144, 30);
+	drawParticles();
+	navigate_to_waypoint(180, 30);
+	drawParticles();
+	navigate_to_waypoint(180, 54);
+	drawParticles();
+	navigate_to_waypoint(164, 54);
+	drawParticles();
+	navigate_to_waypoint(126, 54);
+	drawParticles();
+	// Moved left
+	// Going up
+	navigate_to_waypoint(126, 74);
+	drawParticles();
+	navigate_to_waypoint(126, 94);
+	drawParticles();
+	navigate_to_waypoint(126, 104);
+	drawParticles();
+	navigate_to_waypoint(126, 124);
+	drawParticles();
+	navigate_to_waypoint(126, 144);
+	drawParticles();
+	navigate_to_waypoint(126, 168);
+	drawParticles();
+	navigate_to_waypoint(126, 148);
+	drawParticles();
+	navigate_to_waypoint(126, 126);
+	drawParticles();
+	// From here, move in 20 cm ranges
+	navigate_to_waypoint(30, 54);
+	drawParticles();
+	navigate_to_waypoint(84, 54);
+	drawParticles();
+	navigate_to_waypoint(84, 30);
+	drawParticles();
 
   StopTask(vehicle_compute_position);
   StopTask(vehicle_draw_position);
