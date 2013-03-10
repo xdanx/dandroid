bf893a766eca5d6356a9c4b4d85c6d691ab14adf
matah <selma@apexis.ro>
2013-03-10 17:12:43 +0000

    changed the sign of wall followint

--- practical8/test.c
+++ practical8/test.c
@@ -34,12 +34,16 @@ void enter_cubicle(int dir) {
 
 	float k = (radius - (w / 2.0))/(radius + (w / 2.0));
 
-	wheel = (dir == -1 ? LEFT_WHEEL : RIGHT_WHEEL);
+	wheel = (dir == -1 ? RIGHT_WHEEL : LEFT_WHEEL);
 
 	motor[wheel] *= k;
 
-	wait1Msec(268.65);
+	wait1Msec(1025.65);
 
+	motor[RIGHT_WHEEL] = DEFAULT_POWER;
+	motor[LEFT_WHEEL] = DEFAULT_POWER;
+
+	wait1Msec(300);
 }
 
 // dir = -1 (going east)
@@ -48,22 +52,22 @@ void follow_wall(int distance, int dir) {
 
 	int wheel, diff = 0, current_distance = 0, correction = 0;
 	int count = 0;
-	float k = 2;
+	float k = 1.2;
 
-	while(current_distance < 60 || current_distance > 75) {
+	while(current_distance < 60 || current_distance > 200) {
 		count++;
 		motor[RIGHT_WHEEL] = 80;
 		motor[LEFT_WHEEL] = 80;
 
 		// filter garbage readings
-		if (current_distance > 75) {
+		if (current_distance > 200) {
 			current_distance = SensorValue[sonar];
 			continue;
 		}
 		current_distance = SensorValue[sonar];
-		//writeDebugStream("Wheel %d, motor 1: %d, motor 2: %d \n", wheel, motor[RIGHT_WHEEL], motor[LEFT_WHEEL]);
+		writeDebugStream("Wheel %d, motor 1: %d, motor 2: %d \n", wheel, motor[RIGHT_WHEEL], motor[LEFT_WHEEL]);
 		//wait1Msec(10);
-		//writeDebugStream("Sensor reading: %d \n", current_distance);
+		writeDebugStream("Sensor reading: %d \n", current_distance);
 		//wait1Msec(10);
 
 		//if (count%5 == 0) {
@@ -76,12 +80,12 @@ void follow_wall(int distance, int dir) {
 		// set power of motor to turn towards wall
 		correction = (int) (-k * diff);
 
-		motor[RIGHT_WHEEL] += correction;
-		motor[LEFT_WHEEL] -= correction;
+		motor[RIGHT_WHEEL] -= correction;
+		motor[LEFT_WHEEL] += correction;
 
-		//writeDebugStream("Value of diff: %d , correction: %d, wheel: %d \n", diff, correction, wheel);
+		writeDebugStream("Value of diff: %d , correction: %d, wheel: %d \n", diff, correction, wheel);
 		//wait1Msec(10);
-		//writeDebugStream("Wheel %d, motor 1: %d, motor 2: %d \n", wheel, motor[RIGHT_WHEEL], motor[LEFT_WHEEL]);
+		writeDebugStream("Wheel %d, motor 1: %d, motor 2: %d \n", wheel, motor[RIGHT_WHEEL], motor[LEFT_WHEEL]);
 		//wait1Msec(10);
 
 	//	}
@@ -97,8 +101,8 @@ task main()
 {
 
 	clearDebugStream();
-	follow_wall(21, -1);
-	//enter_cubicle(-1);
+	follow_wall(19, -1);
+	enter_cubicle(-1);
 	//motor[LEFT_WHEEL] = 90;
 	//motor[RIGHT_WHEEL] = 98;
 	//wait1Msec(300);
