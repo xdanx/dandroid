0e0f4a139743a23ec6655cdfc9701c3507c0c516
matah <selma@apexis.ro>
2013-03-10 19:20:15 +0000

    fixed exit. not entirely tho

--- practical8/actions.h
+++ practical8/actions.h
@@ -5,7 +5,7 @@
 #include "space.h"
 #include "robot_state.h"
 #include "logic.h"
-int WALL_COUNT = 20;
+#include "test.c"
 
 // distances
 
@@ -21,23 +21,23 @@ int read_sonar() {
 
 void find_cubicle_exit() {
 	// rotate sonar till you look outside
-
+	// TODO: special case: iullf it points outside but then looks at inside of crap; just make it turn back
 	int rotation_dir = 1;
-	sonar_set_rotation(rotation_dir, false);
+	sonar_set_rotation(rotation_dir, SONAR_SPEED_SLOW);
 	while (true) {
-		wait1Msec(30);
+		//wait1Msec(30);
 		if (read_sonar() > OUTSIDE_OF_CUBICLE_THRESH) break;
 	}
 	sonar_stop();
 
 
 	int prevReading = read_sonar();
-	sonar_set_rotation(rotation_dir, false);
+	sonar_set_rotation(rotation_dir, SONAR_SPEED_SLOW);
 	bool dec_started = false; // distance decrease started
 
   // rotate sonar till you minimize outside distance
   while (true) {
-		wait1Msec(30);
+		//wait1Msec(30);
 		int currentReading = read_sonar();
 		if (!dec_started) {
 			if (currentReading < 	prevReading) dec_started = true;
@@ -47,33 +47,41 @@ void find_cubicle_exit() {
 	  }
 		prevReading = currentReading;
 	}
+	writeDebugStream("find cubicle exit: encodings: %d\n", nMotorEncoder[SONAR_M]);
 	sonar_stop();
 	rotation_dir = -1;
-	sonar_set_rotation(rotation_dir, false);
+	sonar_set_rotation(rotation_dir, SONAR_SPEED_SLOW);
+
+	wait1Msec(200);
+
+	/*
 	while (true) { // try to get back to the smallest reading
-		wait1Msec(30);
+		//wait1Msec(30);
 		if(read_sonar() == prevReading) break;
 	}
+	*/
 
 	sonar_stop();
+
+	wait1Msec(100);
 	int rot_degs = nMotorEncoder[SONAR_M];
-	nxtDisplayString(1, "Sonar : %d", rot_degs);
+	writeDebugStream("Find cubicle exit: Rotating for %d\n",rot_degs);
 
 	// rotate robot in the same direction and reposition the sonar
 	sonar_move_at_async(0, true); // in parallel
 	droid_rotate(rot_degs); //sync
 
-	wait1Msec(10000);
+	//wait1Msec(10000);
 }
 
 // PRE: sonar is at 0 encodings
 void step_out_of_cubicle() {
-	droid_move(50);
+	droid_move(30);
 	// perhaps here already start looking left or right to see where you are by the time you reach the middle
 	while (true) {
 		wait1Msec(30);
 		int reading = read_sonar();
-		if (reading < TUNNEL_WIDTH + 2) break; // you are out in the tunnel
+		if (reading < TUNNEL_WIDTH  / 2) break; // you are out in the tunnel
 	}
 }
 
@@ -82,26 +90,33 @@ void step_out_of_cubicle() {
 // PRE: sonar pointing forward
 void get_left_right(int* left, int* right) {
 	sonar_move_at_sync(90, false); //this turns it to the right
-	nxtDisplayString(1, "Rotation : %d", nMotorEncoder[SONAR_M]);
+	nxtDisplayString(1, "Rotation : %d \n", nMotorEncoder[SONAR_M]);
 	wait1Msec(100);
 	*right = read_sonar();
 	sonar_move_at_sync(-90, false); //this turns it to the left
-	nxtDisplayString(1, "Rotation : %d", nMotorEncoder[SONAR_M]);
+	nxtDisplayString(1, "Rotation : %d \n", nMotorEncoder[SONAR_M]);
 	wait1Msec(100);
 	*left = read_sonar();
 }
 
 void escape_cubicle() {
+	writeDebugStream("---- Entering escape cubicle ---- \n");
 	find_cubicle_exit();
 	// sonar MUST be at 0 at this point
 	step_out_of_cubicle();
 	// still moving at this point !!
+
+	stop();
 	int left, right;
 	get_left_right(&left, &right);
+	int cub = get_cubicle_index(left, right);
+
 	// look forward quickly and stop so you don't bump into the wall !!!
-	sonar_move_at_sync(0, false);
-	while (read_sonar() > TUNNEL_WIDTH / 2) {/*wait for it ...*/}
+	sonar_move_at_sync(0, true);
+	//while (read_sonar() > TUNNEL_WIDTH / 2) {/*wait for it ...*/}
+	writeDebugStream("the cub is %d\n", cub);
 	stop();
+
 	//droid_rotate(90); //sync
 }
 
--- practical8/test_actions.c
+++ practical8/test_actions.c
@@ -14,28 +14,19 @@ task main()
 	//find_cubicle_exit();
 
 	bFloatDuringInactiveMotorPWM = false;
+
+	/*
 	int left = 0;
 	int right = 0;
 	get_left_right(&left, &right);
 	writeDebugStream("left right encoder: %d %d\n", left, right );
 	sonar_move_at_sync(0, true);
-	/*
-	sonar_rotate_encod_sync(90);
 	*/
 
+	escape_cubicle();
+	//droid_rotate(-180);
+	//follow_wall(21,-1);
 
-	/*
-	sonar_move_at_sync(-45,false);
-	writeDebugStream("Immediate sonar motor encod: %d\n", nMotorEncoder[SONAR_M]);
-	wait1Msec(1000);
-	writeDebugStream("sonar motor encod: %d\n", nMotorEncoder[SONAR_M]);
-
-	sonar_move_at_sync(45,false);
-	wait1Msec(1000);
-	writeDebugStream("sonar motor encod: %d\n", nMotorEncoder[SONAR_M]);
-	writeDebugStream("Correcting the sonar to be into position");
-  sonar_fix_at_sync(45,true);
-  */
 
 	wait1Msec(10000);
 	//droid_rotate(90);
